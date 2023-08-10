35528f28d5198d150cebee02e3ce00eb94b24ec2
evg007 <evg007554@yandex.ru>
2023-08-10 13:55:02 +0300

    HW 19.2

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
