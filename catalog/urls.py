359ac9be590ef582a832744db1c2b66be9d423c9
evg007 <evg007554@yandex.ru>
2023-08-16 21:57:40 +0300

    HW 19.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,16 +4,16 @@ from django.contrib import admin
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, categories, products, cart_product
+from catalog.views import index, contacts, CategoryListView, productListView, cart_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('categories/', categories, name='categories'),
+    path('categories/', CategoryListView.as_view(), name='categories'),
     path('admin/', admin.site.urls),
-    path('products/', products, name='products'),
+    path('products/', productListView.as_view(), name='products'),
     path('<int:pk>/product/', cart_product, name='cart_product'),
 ]
   # + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from catalog.models import Product, Category
 def index(request):
@@ -8,12 +10,20 @@ def index(request):
     }
     return render(request, 'catalog/index.html', context)
 
-def categories(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'title':'Категории'
+# def categories(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title':'Категории'
+#     }
+#     return render(request, 'catalog/category_list.html', context)
+
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
+         'title':'Категории'
     }
-    return render(request, 'catalog/categories.html', context)
+
+
 
 def  cart_product(request, pk):
     Product_p= Product.objects.get(pk=pk)
@@ -23,14 +33,30 @@ def  cart_product(request, pk):
     }
     return render(request, 'catalog/product.html',context)
 
-def products(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title':'Все товары'
+# def products(request):
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title':'Все товары'
+#     }
+#     return render(request, 'catalog/products.html',context)
+
+class productListView(ListView):
+    model = Product
+    extra_context = {
+        'title': 'Все товары'
     }
-    return render(request, 'catalog/products.html',context)
-
-
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(category_id=self.kwargs.get('pk') )
+    #     return queryset
+    # def get_context_data(self, *args, **kwargs):
+    #     context_data = super().get_context_data(**kwargs)
+    #
+    #     category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+    #     context_data['category_pk'] = category_item.pk,
+    #     context_data['title'] = f'Продукты категории {category_item.name}',
+    #
+    #     return context_data
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
